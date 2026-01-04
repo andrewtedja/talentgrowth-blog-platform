74cc72eba9c7a87433eadc23d105d5d3116e83fd
Andrew Tedjapratama <andrewtedj@gmail.com>
2026-01-04 15:46:50 +0700

    prod(talent-growth): fix sameSite cors error

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -55,7 +55,7 @@ router.post("/register", async (req, res) => {
 		res.cookie("token", token, {
 			httpOnly: true,
 			secure: process.env.NODE_ENV === "production",
-			sameSite: "lax",
+			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 			maxAge: 7 * 24 * 60 * 60 * 1000,
 		});
 
@@ -107,7 +107,7 @@ router.post("/login", async (req, res) => {
 		res.cookie("token", token, {
 			httpOnly: true,
 			secure: process.env.NODE_ENV === "production",
-			sameSite: "lax",
+			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 			maxAge: 7 * 24 * 60 * 60 * 1000,
 		});
 
